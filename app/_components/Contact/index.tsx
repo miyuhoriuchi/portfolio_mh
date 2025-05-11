e5b549fd4e3b0845445b65f20b8e8ee793c0badd
miyuhoriuchi <ciocolatacasa125@gmail.com>
2025-05-11 20:22:07 +0900

    ソーシャルアイコンのスタイル修正

--- app/_components/Contact/index.tsx
+++ app/_components/Contact/index.tsx
@@ -11,21 +11,39 @@ const Contact = () => {
           rel="noopener noreferrer"
           target="_blank"
         >
-          <Image src="/icon_x.svg" alt="" width={32} height={32} />
+          <Image
+            src="/icon_x.svg"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.socialIcon}
+          />
         </Link>
         <Link
           href="https://www.linkedin.com/in/miyu-horiuchi-4a00a7170/"
           rel="noopener noreferrer"
           target="_blank"
         >
-          <Image src="/icon_in.svg" alt="" width={32} height={32} />
+          <Image
+            src="/icon_in.svg"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.socialIcon}
+          />
         </Link>
         <Link
           href="https://github.com/miyuhoriuchi"
           rel="noopener noreferrer"
           target="_blank"
         >
-          <Image src="/icon_git.svg" alt="" width={32} height={32} />
+          <Image
+            src="/icon_git.svg"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.socialIcon}
+          />
         </Link>
       </div>
     </div>
